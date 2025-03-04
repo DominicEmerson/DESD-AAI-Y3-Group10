50bf13b00150febab4dc73770388f39ddd1a02a0
Sethh7278 <153008479+Sethh7278@users.noreply.github.com>
2025-03-04 01:07:08 +0000

    creating front-end + backend in container

--- desdai3/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Accident, Claim, Driver, Vehicle, Injury
-
-admin.site.register(Accident)
-admin.site.register(Claim)
-admin.site.register(Driver)
-admin.site.register(Vehicle)
-admin.site.register(Injury)

9ae7a88f52f62f665587ae0f582912ebe2dad864
Megan Hargrave-Smith <megan2.hargrave-smith@live.uwe.ac.uk>
2025-03-20 14:26:01 +0000

    Refining comments + documentation

--- claims/models.py
+++ claims/models.py
@@ -34,7 +34,7 @@ class CustomUser(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='enduser')
 
-    objects = CustomUserManager()  # 🔹 Attach the custom manager
+    objects = CustomUserManager()  # Attach the custom manager
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"

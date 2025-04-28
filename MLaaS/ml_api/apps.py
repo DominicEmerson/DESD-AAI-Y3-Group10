e10e4b1b69279ddab157698f2632c095aeef8df3
Bradley Booth <57230586+BradleyBooth@users.noreply.github.com>
2025-04-28 18:07:47 +0100

    rm all from main in prep for refactor changes

--- MLaaS/ml_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class MLApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ml_api'
--- claims/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-
-@receiver(user_logged_in)
-def set_idle_session_expiry(sender, request, user, **kwargs):
-    if hasattr(user, 'role') and user.role == 'enduser':
-        request.session.set_expiry(600)  # 1 minute for testing
-    else:
-        request.session.set_expiry(0)   # Expires when browser closes

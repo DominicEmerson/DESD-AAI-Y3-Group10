e612a32d494a2380bb538e0d41573dccb7cded32
Ahmed <AMohamed980@hotmail.com>
2025-05-02 18:59:35 +0100

    added prediction view in claims and also added a view details button that takes use to claim detail page

--- Backend/claims/urls.py
+++ Backend/claims/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('new/', views.ClaimSubmissionView.as_view(), name='claim_submission'),
     path('success/', views.ClaimSuccessView.as_view(), name='claim_submission_success'),
     path('<int:pk>/prediction/', views.ClaimPredictionView.as_view(), name='claim_prediction'),
+    path('details/<int:claim_id>/', views.claim_detail, name='claim_detail'),
 ]
